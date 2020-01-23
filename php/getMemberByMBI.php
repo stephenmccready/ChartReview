a39fa08553d21e34431867bd072a8c08b8e68121
Stephen McCready <stephenmccready@gmail.com>
2020-01-22 22:52:59 -0500

    Create getMemberByMBI.php

---
+++ php/getMemberByMBI.php
@@ -0,0 +1,21 @@
+<?php
+include ("mysqli.php");
+
+$q="SELECT P.*, M.SourcePersonKey As MemberID, MBI.SourcePersonKey As MBI, HICN.SourcePersonKey As HICN
+FROM factPerson As P 
+JOIN luPersonXref As M on M.MasterPersonID = P.MasterPersonId And M.PersonTypeCode = 'L' And M.SystemSourceCode = 'CT'
+JOIN luPersonXref As MBI on MBI.MasterPersonID = P.MasterPersonId And MBI.PersonTypeCode = 'B'
+JOIN luPersonXref As HICN on HICN.MasterPersonID = P.MasterPersonId And HICN.PersonTypeCode = 'R'
+WHERE MBI.SourcePersonKey = '" . $_GET['mbi'] . "'";
+$result = $connect->query($q);
+
+if ($result->num_rows == 1)
+{
+	while($record = $result->fetch_assoc())
+	{
+		$date=date_create($record['PersonDateOfBirth']);
+		echo $record['ID'] . '|' . $record['PersonLastName']. '|' . $record['PersonFirstName'] . '|' . $record['MemberID'] . '|' . $record['HICN'] . '|' . date_format($date, "Y-m-d") . '|' . $record['MasterPersonID'];
+	}	
+} else {
+	echo 0;
+}
